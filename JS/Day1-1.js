c377bc376be54f0f3f4883116da5b6889380e7ce
BhushanBadhe39 <bhushanbadhe39@gmail.com>
2025-11-05 18:59:02 +0530

    JavaScript Day 1

---
+++ JS/Day1-1.js
@@ -0,0 +1,18 @@
+document.write("Hello JavaScript");
+console.log("Hello Console")
+// alert("Hello!!!")
+
+var a = 20
+var b = 10
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+
+console.log(a<b)
+console.log(a>b)
+console.log(a<=b)
+console.log(a>=b)
+console.log(a==b)
