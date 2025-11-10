3f92bd7f5dff001ab81eec9d5793b79f7a5784da
BhushanBadhe39 <bhushanbadhe39@gmail.com>
2025-11-10 19:04:20 +0530

    JavaScript Day 2 and 3

---
+++ JS/Day2-1.js
@@ -0,0 +1,109 @@
+console.log("Hello Log....")
+
+var a=20;
+var b=30;
+var c=40;
+
+console.log(b>a && c>b);  
+
+console.log ("Hello JS......")
+
+var a=30.5; // float
+var b=40; // int
+var c=50;
+
+console.log(a>b || c<b);  
+
+var a= 40;
+var b= 50;
+var c= 60;
+
+//syntax :
+
+// typeof(variable)
+
+console.log(typeof(a));
+
+
+console.log(! c<a); 
+
+console.log(! a>c); 
+
+// comment in js 
+
+// comments Are Unexecutable Part Of The Program.
+
+// we Can Define Comments in JS Using "//" 
+
+console.log(typeof(b));
+console.log(typeof(c));
+console.log(typeof(d));
+
+// '' we can use both ""
+
+var a="BHUSHAN";
+
+console.log(typeof(a));
+
+console.log(a);
+
+var a='Pathaan';
+
+console.log(typeof(a))
+
+console.log(a)
+
+var isLogin=true;
+
+var x=false;
+
+console.log(typeof(x))
+
+console.log(typeof(isLogin))
+
+var z=false;
+
+console.log(typeof(z))
+
+var n1;
+
+console.log(typeof(n1))
+
+var m1;
+
+console.log(typeof(m1)) 
+
+var n1=null;
+
+console.log(typeof(n1))
+
+var m1=null;
+
+console.log(typeof(m1)) 
+
+var n2=100;
+
+var n2=400;
+
+console.log(n2) 
+
+let n3=50;
+
+n3 = 105;
+
+console.log(n3)
+
+let m1=25;
+
+m1=75;
+
+console.log(m1) 
+
+const k1=200;
+
+console.log(k1)
+
+const l=300;
+
+
+console.log(l)
---
+++ JS/Day3-1.js
@@ -0,0 +1,21 @@
+// let a=10;
+// let b=20;
+// let c=30;
+// console.log(a+b+c);
+// console.log("Addition of",a,b,"and" , c, "is" ,a+b+c);
+
+
+
+//--------------------------------------------------------------------------------------//
+//                                 // IF ELSE STATEMENT                                 //
+//--------------------------------------------------------------------------------------//
+
+let a=10;
+if (a>5){
+    console.log("a is greater than 5");
+    
+}
+else{
+    console.log("a is less than 5");
+}
+console.log("Hello World!!!");
