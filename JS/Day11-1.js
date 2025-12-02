3180de5130092da8a64f085d9900e972aa7ba517
BhushanBadhe39 <bhushanbadhe39@gmail.com>
2025-12-02 19:11:42 +0530

    JavaScript Day 12

--- JS/Day11-1.js
+++ JS/Day11-1.js
@@ -45,20 +45,68 @@ while(index_arr1!=arr1.length){
 
     console.log(arr1.shift());
     console.log(arr1);
-  
 
+    // 6 - At() --> used to return element at specific position.
+
+    console.log(arr1.at(3));
+    
+    // 7 - concat() 
+
+    const arr2 = [101,202,303];
+    arr1.concat(arr2);
+    console.log(arr1);
+    
+    // 8 - slice(start,end)
+
+    console.log(arr1.slice(0,3));
+    
+    // 9 - splice() -->  Removes elements from an array and, if necessary, inserts new elements in their place, returning the deleted elements.
+
+    arr1.splice(1,1,101);
+    console.log(arr1);
+    // OR 
+    arr1.splice(1,2,101,102);
+    console.log(arr1);
+    
+    // 10 - join() -->  used to join given list by using seperator and generate new string
+
+    const strArr = ["JavaScript","is","Programming","Language"];
+    let strArr2 = strArr.join(" ");
+    console.log(strArr2);
+
+    // 11 - map() --> Calls a defined callback function on each element of an array, and returns an array that contains the results.
+
+    const arr3 = [10,20,30,40,50];
+    const arr4 = arr3.map((n)=>n*n);
+    console.log(arr4);
+    
+    // 12 - filter() --> used to filter out values from array based on condition and henerate new array.
+
+    const arr7 = arr3.filter((n)=> n % 20 == 0);
+    console.log(arr7);
+    
+    // 13 - reduce() --> used to perform operations on array elements in pairs till a single value is achieved.
+
+    const sum = arr3.reduce((a,b)=>a+b);
+    console.log(sum);
+
+    // 14 - For of Loop
+
+    for (const element of arr3) {
+        console.log(element);
+        
+    }
+    
+    
 //--------------------------------------------------------------------------------------//
 //                                       PRACTICE                                       //
 //--------------------------------------------------------------------------------------//
-
-arr1.unshift("hii!");
-console.log(arr1);
 
 
 //                   Create an array of even numbers from 0 to 100                    //
 console.log("\n\n\n\nCreate an array of even numbers from 0 to 100");
 
-let evenArr = [];
+const evenArr = [];
 for(let i=0;i<=100;i+=2){
     evenArr.push(i);
 }
@@ -67,7 +115,7 @@ console.log(evenArr);
 //                  WAP to print array of prime numbers from 1 to 100                   //
 console.log("\n\n\n\nWAP to print array of prime numbers from 1 to 100");
 
-let primeArr = [];
+const primeArr = [];
 for(let i=2;i<=100;i++){
     let count=0;
     for(let j =1;j<=i;j++){
@@ -83,19 +131,77 @@ console.log(primeArr);
 
 
 
+//                      WAP to remove duplicate values from array.                      //
+console.log("\n\n\n\nWAP to remove duplicate values from array.");
 
 
-let primeArr2 = [];
 
-for(let i = 2;i<=100;i++){
-    let count = 0;
-    for(let j =0;j<=i;j++){
-        if(i%j==0){
-            count++;
-        }
-    }
-    if(count==2){
-        primeArr2.push(i);
+const givenArr = [1,2,3,4,5,1,4,12];
+let checkArr = [];
+
+// for(let i=0;i<givenArr.length;i++){
+//     if(checkArr.includes(givenArr[i])){
+//         continue;
+//     }
+//     else{
+//         checkArr.push(givenArr[i]);
+//     }
+// }
+// console.log(checkArr);
+
+
+for(let i=0;i<givenArr.length;i++){
+    if(!checkArr.includes(givenArr[i])){
+        checkArr.push(givenArr[i]);
     }
 }
-console.log(primeArr2);
+console.log(checkArr);
+
+
+//                          WAP to print count of 10 in array                           //
+console.log("\n\n\n\nWAP to print count of 10 in array");
+
+
+const sampleArr = [10,20,1,15,30,10];
+let sampleNum = 10;
+// let sampleNum = prompt("enter");
+let count =0;
+for(let i = 0;i<sampleArr.length;i++){
+    if(sampleArr[i]==sampleNum){
+        count++;
+    }
+}
+console.log(count);
+
+//                      WAP to find the largest number in an array                      //
+console.log("\n\n\n\nWAP to find the largest number in an array");
+
+const sampleArr2 = [10,5,30,7,22];
+let largest = sampleArr2[0];
+for(let i=1;i<sampleArr2.length;i++){
+    if(sampleArr2[i]>largest){
+        largest=sampleArr2[i];
+    }
+}
+console.log(largest);
+
+
+
+//                 WAP to print Square of even Numbers from given array                 //
+console.log("\n\n\n\nWAP to print Square of even Numbers from given array");
+
+
+const arr5 = [1,2,3,4,5,6,7,8,9,10];
+const arr6 =[];
+for(let i=0;i<arr5.length;i++){
+    if(arr5[i]%2==0){
+        arr6.push(arr5[i]*arr5[i]);
+        console.log(arr5[i]**2);
+    }
+}
+console.log(arr6);
+
+//OR
+
+const arr8 = arr5.filter((n)=>n%2==0).map((n)=>n**2);
+console.log(arr8);
---
+++ JS/Day12-1.js
@@ -0,0 +1,126 @@
+//--------------------------------------------------------------------------------------//
+//                                                                                      //
+//                                       OBJECTS                                        //
+//                                                                                      //
+//--------------------------------------------------------------------------------------//
+
+//--------------------------------------------------------------------------------------//
+//                       Object is a collection key-value pairs.                        //
+//--------------------------------------------------------------------------------------//
+
+
+
+//                            We can Define Object using...                             //
+
+    // 1. Curly Bracket
+    
+    let student = {name:"Bhushan",age:20,roll:101};
+    console.log(student);
+    console.log(typeof(student));
+    console.log(student.name);
+    
+    // 2. Object Constructor
+
+    const person = new Object();
+    person.name = "Prathamesh";
+    person.mobile = [1234,7498600952];
+    person.address = {city:"Pune",state: "Maharashtra"};
+    console.log(person);
+    
+
+
+
+//                                  Accessing Objects                                   //
+console.log("\n\n\n\n\n\n\n\n\n\n");
+
+
+    // i. Using Key name
+    console.log(student.name);
+    console.log(student.age);
+    console.log(student.roll);
+    
+    // ii. Using Double or single quotes in sq. braces
+    console.log(student["name"]);
+    console.log(student['age']);
+    
+    // iii. For in Loop
+
+    for (const key in person) {
+        console.log(key);
+        console.log(person[key]);
+    }
+
+
+//                        Built-in Methods/Commands for objects                        //
+console.log("\n\n\n\n\n\n\n\n\n");
+
+
+// Update values
+
+student.name = "Shri"
+student['roll'] = 12
+console.log(student);
+
+
+// Delete key-value pair
+
+delete student.roll;
+console.log(student);
+
+
+// Delete Object
+
+student = null;
+console.log(student);
+
+
+// Keys ---> Returns array of all keys in an object
+
+console.log(Object.keys(person));
+
+
+// Values ---> Returns array of all values in an object
+
+console.log(Object.values(person));
+
+
+// Entries ---> returns array of key-value pairs from an object
+
+console.log(Object.entries(person));
+
+
+// For of Loop
+
+for (const element of Object.keys(person)) {
+    console.log(element);
+    
+}
+
+
+
+
+
+//--------------------------------------------------------------------------------------//
+//                                                                                      //
+//                                       Practice                                       //
+//                                                                                      //
+//--------------------------------------------------------------------------------------//
+console.log("\n\n\n\n\n\n\n\n\n\n\n\n");
+
+
+
+let l_details = new Object();
+l_details.name = "Bhushan";
+l_details.batch = "P-221";
+l_details.html = 82;
+l_details.css = 78;
+l_details.javascript = 93;
+console.log(l_details);
+
+//                              WAP to print avg of marks                               //
+
+l_details.total = l_details.html+l_details.css+l_details.javascript;
+l_details.avg = parseInt(l_details.total/3);
+console.log(l_details.avg+" is the Average marks attained by "+ l_details.name+" from batch "+l_details.batch);
+console.log(l_details);
+
